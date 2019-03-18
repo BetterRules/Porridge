4a6af9265b5c8ee19fefb477f93dbe073f9c406b
Dana Iti <dana.iti.work@gmail.com>
2019-03-19 12:06:59 +1300

    added eligibility icons, and styled components for easier styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import Profile from './components/Profile';
+import Eligibility from './components/Eligibility';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Profile />
+        <Eligibility status={true} />
+        <Eligibility status={false} />
       </div>
     );
   }
---
+++ src/components/Eligibility.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import styled from 'styled-components';
+import CheckIcon from '@material-ui/icons/Check';
+import ClearIcon from '@material-ui/icons/Clear';
+
+const Wrapper = styled.div`
+  border: 1px solid #000;
+  padding: 1rem;
+  width: 1.5625rem;
+
+  svg {
+    font-size: 26px;
+  }
+`;
+
+const Eligibility = props => {
+  return (
+    <Wrapper>
+      {props.status && <CheckIcon />}
+      {!props.status && <ClearIcon />}
+    </Wrapper>
+  );
+}
+
+export default Eligibility;
