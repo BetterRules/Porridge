5f0b799c6b783ab4195e410ea524feaf2cf62399
Mischa Saunders <mischa.saunders@gmail.com>
2019-03-19 13:45:24 +1300

    remove unused vars

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import './App.css';
 import Profile from './components/Profile';
 import Inputs from './components/Inputs';
-import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Eligibility from './components/Eligibility';
 
