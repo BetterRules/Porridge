3c20744855f3faaa54b7686f832c316b119b5f7c
Mischa Saunders <mischa.saunders@gmail.com>
2019-03-19 13:28:07 +1300

    add inputs

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 import Profile from './components/Profile';
+import Inputs from './components/Inputs';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <Paper className="App">
         <Profile />
-      </div>
+        <Inputs />
+      </Paper>
     );
   }
 }
---
+++ src/components/Inputs.js
@@ -0,0 +1,45 @@
+import React, { Fragment, useState } from 'react';
+import TextField from '@material-ui/core/TextField';
+
+const importantDates = [
+  'Date of Birth',
+  'Date of Injury', 
+  'Start of full-time study or training',
+]
+
+const Inputs = () => {
+  const [name, setName] = useState('Onyx');
+  
+  const handleChange = name => event => {
+    setName(event.target.value)
+  };
+  
+  return (
+    <form className="Form-Date" noValidate autoComplete="off">
+      <TextField
+          id="name"
+          label="Name"
+          value= {name}
+          onChange={handleChange("name")}
+          margin="normal"
+          variant="filled"
+          className="Input-Date"
+      />
+      {importantDates.map(label => (
+        <TextField
+          id={label}
+          label={label}
+          type="date"
+          variant="filled"
+          margin="normal"
+          className="Input-Date"
+          InputLabelProps={{
+          shrink: true,
+          }}
+        />
+      ))}
+    </form>            
+  );
+}
+
+export default Inputs;
