7262ed16fcefdfb2ae0b19be90d405f286599e6a
Dana Iti <dana.iti.work@gmail.com>
2019-03-19 14:03:09 +1300

    added axios. added config and OF data file

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 import Profile from './components/Profile';
 import Eligibility from './components/Eligibility';
-
 class App extends Component {
+
   render() {
     return (
       <div className="App">
