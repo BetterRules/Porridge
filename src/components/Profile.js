5b6ed9f907f8a18e191a3ad68775d3776cebc2b6
Mischa Saunders <mischa.saunders@gmail.com>
2019-03-19 13:44:22 +1300

    remove unused var

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,6 +1,5 @@
 import React, { Fragment } from 'react';
 import Avatar from '@material-ui/core/Avatar';
-import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 
