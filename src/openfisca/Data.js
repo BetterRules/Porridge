f4f9c633b3920a29b3f8fa623be6ca523a3605e8
Dana Iti <dana.iti.work@gmail.com>
2019-03-19 13:32:29 +1300

    incomplete data to be sent to OF

---
+++ src/openfisca/Data.js
@@ -0,0 +1,21 @@
+let DATA = {
+  persons: {
+    Tahi: {
+      is_studying_full_time: {
+        '2018-08': true,
+      },
+    },
+  },
+  titled_properties: {
+    house: {
+      others: ['Tahi'],
+    },
+  },
+  families: {
+    family: {
+      others: ['Tahi'],
+    },
+  }
+};
+
+export default DATA;
