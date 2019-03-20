e1dfc922318850008bcbadbc48f320c8d22cbbe2
Dana Iti <dana.iti.work@gmail.com>
2019-03-21 10:50:53 +1300

    tidy up data structure

--- src/openfisca/Data.js
+++ src/openfisca/Data.js
@@ -1,139 +1,61 @@
 const DATA = [
-    {
-      firstName: 'Freddy',
-      lastName: 'Mercury',
-      acc__earner: {
-        "2010-08-03": true
-      },
-      acc__has_cover: {
-        "2010-08-03": true
-      },
-      acc_part_2__suffered_personal_injury: {
-        "2010-08-03": true
-      },
-      acc_part_3__has_lodged_claim: {
-        "2010-08-03": true
-      },
-      acc_sched_1__engaged_fulltime_study_or_training: {
-        "2010-08-03": true
-      },
-      acc_sched_1__weekly_earnings: {
-        "2010-08-03": 1000
-      },
-      acc_sched_1__incapacitated_for_6_months: {
-        "2010-08-03": true
-      },
-      date_of_birth: {
-        '1998-11-11': true
-      },
-      date_of_injury: {
-        '2005-04-04': true
-      },
-      incapacity_for_employment__corporation_determination: {
-        "2010-08-03": true
-      },
-      incapacity_for_employment__by_covered_injury: {
-        "2010-08-03": true
-      },
-      finish_date_of_full_time_study_training_bridging_18th_birthday: {
-        "2020-08-05": true
-      }, 
-      acc_sched_1__lope_eligible: {
-        '2018-04-11': null
-      }
-       // engaged_fulltime_study_or_training: '2007-07-07'
-      // minimum_wage___minimum_adult_rate_wages: true,
-   },
-    {
-      firstName: 'Taika',
-      lastName: 'Waititi',
-      acc__earner: {
-        "2010-08-03": true
-      },
-      acc__has_cover: {
-        "2010-08-03": true
-      },
-      acc_part_2__suffered_personal_injury: {
-        "2010-08-03": true
-      },
-      acc_part_3__has_lodged_claim: {
-        "2010-08-03": true
-      },
-      acc_sched_1__engaged_fulltime_study_or_training: {
-        "2010-08-03": true
-      },
-      acc_sched_1__weekly_earnings: {
-        "2010-08-03": 1000
-      },
-      acc_sched_1__incapacitated_for_6_months: {
-        "2010-08-03": true
-      },
-      date_of_birth: {
-        '1998-11-11': true
-      },
-      date_of_injury: {
-        '2005-04-04': true
-      },
-      incapacity_for_employment__corporation_determination: {
-        "2010-08-03": true
-      },
-      incapacity_for_employment__by_covered_injury: {
-        "2010-08-03": true
-      },
-      finish_date_of_full_time_study_training_bridging_18th_birthday: {
-        "2020-08-05": true
-      }, 
-      acc_sched_1__lope_eligible: {
-        '2018-04-11': null
-      }
-       // engaged_fulltime_study_or_training: '2007-07-07'
-      // minimum_wage___minimum_adult_rate_wages: true,
-    },
-    {
-      firstName: 'Greta',
-      lastName: 'Thunberg',
-      acc__earner: {
-        "2010-08-03": true
-      },
-      acc__has_cover: {
-        "2010-08-03": true
-      },
-      acc_part_2__suffered_personal_injury: {
-        "2010-08-03": true
-      },
-      acc_part_3__has_lodged_claim: {
-        "2010-08-03": true
-      },
-      acc_sched_1__engaged_fulltime_study_or_training: {
-        "2010-08-03": true
-      },
-      acc_sched_1__weekly_earnings: {
-        "2010-08-03": 1000
-      },
-      acc_sched_1__incapacitated_for_6_months: {
-        "2010-08-03": true
-      },
-      date_of_birth: {
-        '1998-11-11': true
-      },
-      date_of_injury: {
-        '2005-04-04': true
-      },
-      incapacity_for_employment__corporation_determination: {
-        "2010-08-03": true
-      },
-      incapacity_for_employment__by_covered_injury: {
-        "2010-08-03": true
-      },
-      finish_date_of_full_time_study_training_bridging_18th_birthday: {
-        "2020-08-05": true
-      }, 
-      acc_sched_1__lope_eligible: {
-        '2018-04-11': null
-      }
-       // engaged_fulltime_study_or_training: '2007-07-07'
-      // minimum_wage___minimum_adult_rate_wages: true,
-    }
+  {
+    firstName: 'Freddy',
+    lastName: 'Mercury',
+    acc__earner: true,
+    acc__has_cover: true,
+    acc_part_2__suffered_personal_injury: true,
+    acc_part_3__has_lodged_claim: true,
+    acc_sched_1__engaged_fulltime_study_or_training: true,
+    acc_sched_1__weekly_earnings: 1000,
+    acc_sched_1__incapacitated_for_6_months: true,
+    date_of_birth: '1998-11-11',
+    date_of_injury: '2005-04-04',
+    incapacity_for_employment__corporation_determination: true,
+    incapacity_for_employment__by_covered_injury: true,
+    finish_date_of_full_time_study_training_bridging_18th_birthday: true,
+    acc_sched_1__lope_eligible: null,
+    // engaged_fulltime_study_or_training: '2007-07-07'
+    // minimum_wage___minimum_adult_rate_wages: true,
+  },
+  {
+    firstName: 'Taika',
+    lastName: 'Waititi',
+    acc__earner: true,
+    acc__has_cover: true,
+    acc_part_2__suffered_personal_injury: true,
+    acc_part_3__has_lodged_claim: true,
+    acc_sched_1__engaged_fulltime_study_or_training: true,
+    acc_sched_1__weekly_earnings: 1000,
+    acc_sched_1__incapacitated_for_6_months: true,
+    date_of_birth: '1998-11-11',
+    date_of_injury: true,
+    incapacity_for_employment__corporation_determination: true,
+    incapacity_for_employment__by_covered_injury: true,
+    finish_date_of_full_time_study_training_bridging_18th_birthday: true,
+    acc_sched_1__lope_eligible: null
+    // engaged_fulltime_study_or_training: '2007-07-07'
+    // minimum_wage___minimum_adult_rate_wages: true,
+  },
+  {
+    firstName: 'Greta',
+    lastName: 'Thunberg',
+    acc__earner: true,
+    acc__has_cover: true,
+    acc_part_2__suffered_personal_injury: true,
+    acc_part_3__has_lodged_claim: true,
+    acc_sched_1__engaged_fulltime_study_or_training: true,
+    acc_sched_1__weekly_earnings: 1000,
+    acc_sched_1__incapacitated_for_6_months: true,
+    date_of_birth: '1998-11-11',
+    date_of_injury: '2005-04-04',
+    incapacity_for_employment__corporation_determination: true,
+    incapacity_for_employment__by_covered_injury: true,
+    finish_date_of_full_time_study_training_bridging_18th_birthday: true,
+    acc_sched_1__lope_eligible: null
+    // engaged_fulltime_study_or_training: '2007-07-07'
+    // minimum_wage___minimum_adult_rate_wages: true,
+  }
 ]
 
 export default DATA;
