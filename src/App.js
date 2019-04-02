f374b5a9c29ec71c2619c6bf564b233c7566132a
Hamish <hamish@verb.co.nz>
2019-04-03 11:56:11 +1300

    Initial setup for displaying graphs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
 import AccordionItem from './containers/Accordion-Item';
+import Simulation from './containers/Simulation';
 import Paper from '@material-ui/core/Paper';
 import DATA from './openfisca/Data'
 
 const App = () => {
   return (
     <Paper className="App">
+      <Simulation />
+      <div style={{marginTop: '2em'}}>
       {DATA.map(example => {
         return (
         <AccordionItem
@@ -15,6 +18,7 @@ const App = () => {
         />
       )}
       )}
+      </div>
     </Paper>
   );
 }
