e61f35b95ae23837d4eccae0d7312200cc1ba11a
Dana Iti <dana.iti.work@gmail.com>
2019-04-08 13:51:19 +1200

    Add selected bar values

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -5,23 +5,49 @@ import groupAndCount from '../utilities/groupAndCount';
 
 const Chart = props => {
 
-  const agesObj = groupAndCount(props.data, 'age');
+  function getGroup(data, filter) {
+    const group = groupAndCount(data, filter);
+    const obj = [];
+    Object.values(group).map(item => {
+      obj.push([item[filter] * 1, item.count])
+    })
 
-  console.log(agesObj)
+    return obj;
+  }
+
+  function getValues(obj, index) {
+    return Object.values(obj).map(item => item[index])
+  }
+
+  const trace2Group = [];
+
+  getGroup(props.data, 'age').map(item => {
+    if(item[0] === (props.selectedAge * 1)) {
+      trace2Group.push([item[0], item[1]])
+    }
+  })
 
   var trace1 = {
-    x: _.range((props.selectedAge + 1) * 1),
-    y: _.range(19), // counts of each age
+    x: getValues(getGroup(props.data, 'age'), 0),
+    y: getValues(getGroup(props.data, 'age'), 1), // counts of each age
     name: 'Ages',
     type: 'bar'
   }
+  
+  var trace2 = {
+    x: getValues(trace2Group, 0),
+    y: getValues(trace2Group, 1), // counts of each age
+    name: 'Selected Age',
+    type: 'bar'
+  }
+
   return (
     <div>
       <Plot
         data={[
-          trace1
+          trace1, trace2
         ]}
-        layout={ {barmode: 'group', title: 'Seriousness'} }
+        layout={ {barmode: 'group', title: 'Ages'} }
       />
     </div>
   );
